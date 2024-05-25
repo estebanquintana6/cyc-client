89e2731b508acc0204211841cacb620a9ecf0106
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-05-25 17:42:54 -0600

    header

--- src/components/Header/Main/Main.jsx
+++ src/components/Header/Main/Main.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
+// url('../public/img/c_web_slider_connie.jpeg')
+
 const Main = () => {
     return (
-        <div className="bg-deep-purple-accent-700 h-screen flex">
+        <div className="bg-cover h-screen flex"
+        style={{
+            backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.3)), url('/img/c_web_slider_connie.jpeg')"
+        }}>
             <div className="px-4 xs:py-32 sm:py-16 mx-auto my-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
                     <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
                         <a href="/" className="mb-6 mt-auto xs:mx-auto">
-                            <img src="/img/logo.png" className="xs:w-64 sm:w-96" />
+                            <img src="/img/logo.png" className="xs:w-64 sm:w-96 animate-spin" />
                         </a>
                         <div className="max-w-xl mb-auto md:mx-auto text-center lg:max-w-2xl">
                             <h2 className="max-w-lg mb-6 font-sans xs:text-5xl sm:text-6xl font-bold leading-none tracking-tight text-white uppercase md:mx-auto">
                                 Connieyepiz
                             </h2>
-                            <p className="text-base text-indigo-100 md:text-lg">
+                            <p className="text-2xl text-white">
                                 Cada experiencia se encuentra conectada a los espacios en que suceden
                             </p>
                         </div>
