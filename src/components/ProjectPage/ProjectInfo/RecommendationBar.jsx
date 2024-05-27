be6d871e49bc979c33a8848c4cee238dcc6704eb
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-05-26 20:49:38 -0600

    adjusting recommended projects

--- src/components/ProjectPage/ProjectInfo/RecommendationBar.jsx
+++ src/components/ProjectPage/ProjectInfo/RecommendationBar.jsx
@@ -3,7 +3,7 @@ const RecommendationBar = ({ projects = [] }) => {
   <div class="grid grid-cols-2 gap-6 xs:grid-cols-2 lg:grid-cols-4">
       {projects.map((project) => (
           <div className="col-span-1 flex flex-col text-center bg-white">
-            <div className="xs:h-48 xs:w-48 sm:h-64 sm:w-64">
+            <div className="xs:h-40 xs:w-40 sm:h-64 sm:w-64">
               <img className="h-full w-full object-cover object-center" src="https://flowbite-react.com/images/blog/image-4.jpg" alt="project" />
             </div>
         </div>
