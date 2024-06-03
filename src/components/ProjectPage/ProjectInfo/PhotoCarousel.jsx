62ee004893a6fe669b27148f09bc2a467d79fbaf
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-06-02 21:49:49 -0600

    fixing carousel

--- src/components/ProjectPage/ProjectInfo/PhotoCarousel.jsx
+++ src/components/ProjectPage/ProjectInfo/PhotoCarousel.jsx
@@ -2,10 +2,10 @@ import { Carousel } from "flowbite-react";
 
 const PhotoCarousel = ({ photos = [] }) => {
   return (
-    <div className="h-56 xs:h-96 sm:h-full">
-        <Carousel slide={false} className="min-h-[400px]">
+    <div className="h-56 xs:h-96 sm:h-full w-full">
+        <Carousel slide={false} className="xs:min-h-[300px] xs:max-h-[300px] sm:min-h-[400px] sm-max-h-[400px] w-full">
         {photos.map((url) => (
-            <img src={url} alt="proyecto recomendado" className="h-full" key={url} />
+            <img src={url} alt="proyecto recomendado" className="h-full w-full" key={url} />
         ))}
         </Carousel>
     </div>
