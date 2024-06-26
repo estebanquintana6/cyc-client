3fb346fc31601d7a61d01ad7155f5941aef0acb0
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-06-26 13:31:50 -0600

    fixing recommendations

--- src/components/Blog/BlogEntry.jsx
+++ src/components/Blog/BlogEntry.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
 import { fetch } from "../../utils/authFetch";
 
 const dateOptions = {
