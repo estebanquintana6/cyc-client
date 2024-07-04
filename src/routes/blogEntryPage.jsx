44143573d86e36f40e3f146ae0d927490e24719f
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-07-03 18:48:26 -0600

    fixing scroll

--- src/routes/blogEntryPage.jsx
+++ src/routes/blogEntryPage.jsx
@@ -3,8 +3,14 @@ import { useParams } from "react-router-dom";
 import BlogEntry from "../components/Blog/BlogEntry";
 import ContactSection from "../components/LandingPage/Contact/ContactSection";
 import Nav from "../components/ProjectPage/Nav/Nav";
+import { useEffect } from "react";
 
 const BlogEntryPage = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
     const { id } = useParams();
     return (
         <>
