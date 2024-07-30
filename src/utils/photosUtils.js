493c043ea57108382c99ce0ea7b3b127cf071f15
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-07-30 13:32:39 -0600

    fix

--- src/utils/photosUtils.js
+++ src/utils/photosUtils.js
@@ -1,6 +1,6 @@
 export const getFirstPhoto = (photos = []) => {
 
-  if (photos.length == 1) return 0;
+  if (photos.length == 1 || photos.length == 0) photos[0];
 
   return (photos || []).reduce((prev, curr) =>
     (prev?.position ?? Number.MAX_SAFE_INTEGER) <
