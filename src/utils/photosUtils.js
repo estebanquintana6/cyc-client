62691aa7bdee8f65478dff95229f76c9f774cf48
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-07-30 13:28:20 -0600

    fix

--- src/utils/photosUtils.js
+++ src/utils/photosUtils.js
@@ -1,7 +1,7 @@
 export const getFirstPhoto = (photos = []) => {
-  return photos.reduce((prev, curr) =>
-    (prev.position ?? Number.MAX_SAFE_INTEGER) <
-    (curr.position ?? Number.MAX_SAFE_INTEGER)
+  return (photos || []).reduce((prev, curr) =>
+    (prev?.position ?? Number.MAX_SAFE_INTEGER) <
+    (curr?.position ?? Number.MAX_SAFE_INTEGER)
       ? prev
       : curr,
   );
